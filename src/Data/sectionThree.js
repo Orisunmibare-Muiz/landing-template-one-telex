d434ba10570074de91e1e294143a9e521965b76b
Orisunmibare Muiz Olamilekan <orismuiz2003@gmail.com>
2026-01-08 16:38:04 +0100

    feat: add secThree map data to section

---
+++ src/Data/sectionThree.js
@@ -0,0 +1,35 @@
+import clockIcon from "/Images/clock icon.svg";
+import zoom from "/Images/zoom icon.jpg";
+import settingsIcon from "../../public/Images/settings icon.jpg"
+
+export const sectionThree = [
+  {
+    id: 1,
+    image: clockIcon,
+    alternative: "Clock Icon",
+    header: "Seamless Integration",
+    paragraph: "Connects with CRMs, live chat software, and support tools.",
+  },
+  {
+    id: 2,
+    image: clockIcon,
+    alternative: "Clock Icon",
+    header: "24/7 Availability",
+    paragraph:
+      "Always online for instant assistance, enhancing productivity and ensuring support whenever you need it.",
+  },
+  {
+    id: 3,
+    image: zoom,
+    alternative: "Zoom Icon",
+    header: "Instant AI-Driven Responses",
+    paragraph: " Bob answers customer queries in real time.",
+  },
+  {
+    id: 4,
+    image: settingsIcon,
+    alternative: "Settings Icon",
+    header: "Automate Support Workflows",
+    paragraph: "Handles repetitive inquiries and routes complex cases to human agents.",
+  },
+];
