b8a3ca83d629e08b9ceb17d736ddb33fb85ebd07
Orisunmibare Muiz Olamilekan <orismuiz2003@gmail.com>
2026-01-16 01:05:12 +0100

    lint: add alt attributes to icon images in footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -14,75 +14,84 @@ export default function Footer() {
             <div>
               <img src={logo} alt="Logo images" className="pr-30 pl-5" />
             </div>
-            
+
             <table className="">
-              <tr>
-                <th>Product</th>
-                <th>Marketing</th>
-                <th>Support</th>
-              </tr>
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Marketing</th>
+                  <th>Support</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Application Performance Monitoring</td>
+                  <td>Product Managers</td>
+                  <td>Help Center</td>
+                </tr>
 
-              <tr>
-                <td>Application Performance Monitoring</td>
-                <td>Product Managers</td>
-                <td>Help Center</td>
-              </tr>
+                <tr>
+                  <td>Server Monitoring</td>
+                  <td>Software Engineers</td>
+                  <td>FAQ</td>
+                </tr>
 
-              <tr>
-                <td>Server Monitoring</td>
-                <td>Software Engineers</td>
-                <td>FAQ</td>
-              </tr>
+                <tr>
+                  <td>Cloud Monitoring</td>
+                  <td>Cyber Security Analyst</td>
+                  <td>How Telex Works</td>
+                </tr>
 
-              <tr>
-                <td>Cloud Monitoring</td>
-                <td>Cyber Security Analyst</td>
-                <td>How Telex Works</td>
-              </tr>
+                <tr>
+                  <td>Database Monitoring</td>
+                  <td>Small Business Owners</td>
+                  <td>Pricing Experience</td>
+                </tr>
 
-              <tr>
-                <td>Database Monitoring</td>
-                <td>Small Business Owners</td>
-                <td>Pricing Experience</td>
-              </tr>
+                <tr>
+                  <td>Networking Monitoring</td>
+                  <td>Social Media Manager</td>
+                  <td>About Us</td>
+                </tr>
 
-              <tr>
-                <td>Networking Monitoring</td>
-                <td>Social Media Manager</td>
-                <td>About Us</td>
-              </tr>
+                <tr>
+                  <td>Webhook Testing</td>
+                  <td>DevOps</td>
+                  <td>Contact Us</td>
+                </tr>
 
-              <tr>
-                <td>Webhook Testing</td>
-                <td>DevOps</td>
-                <td>Contact Us</td>
-              </tr>
+                <tr>
+                  <td>Push Notification</td>
+                  <td>Data Administrator</td>
+                  <td></td>
+                </tr>
 
-              <tr>
-                <td>Push Notification</td>
-                <td>Data Administrator</td>
-              </tr>
+                <tr>
+                  <td>Uptime Monitoring</td>
+                  <td>Landing Page</td>
+                  <td></td>
+                </tr>
 
-              <tr>
-                <td>Uptime Monitoring</td>
-                <td>Landing Page</td>
-              </tr>
+                <tr>
+                  <td>Website Testing</td>
+                  <td></td>
+                  <td></td>
+                </tr>
 
-              <tr>
-                <td>Website Testing</td>
-              </tr>
-
-              <tr>
-                <td>Integrations</td>
-              </tr>
+                <tr>
+                  <td>Integrations</td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
             </table>
           </div>
           <div className="flex flex-row justify-between lg:w-291.75 mx-auto">
             <div className="flex flex-row gap-4">
-              <img src={instagram} alt="" />
-              <img src={twitter} alt="" />
-              <img src={facebook} alt="" />
-              <img src={linkedIn} alt="" />
+              <img src={instagram} alt="instagram icon" />
+              <img src={twitter} alt="twitter icon" />
+              <img src={facebook} alt="facebook icon" />
+              <img src={linkedIn} alt="linkedin icon" />
             </div>
 
             <div className="flex flex-row gap-6 font-semibold">
